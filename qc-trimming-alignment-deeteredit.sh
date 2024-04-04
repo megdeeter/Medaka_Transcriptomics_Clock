2969fdbbf9fb4dfe4666a33f42a3c78233d066ab
megdeeter <megdeeter@gmail.com>
2024-04-04 14:56:58 -0400

    Update qc-trimming-alignment-deeteredit.sh
    1. Adding #'s temporarily to silence code prior to trimming
    2. Added trimming code, fastqc, and multiqc of trimmed sequences

--- qc-trimming-alignment-deeteredit.sh
+++ qc-trimming-alignment-deeteredit.sh
@@ -10,14 +10,9 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --array=1-85 
 
-#QUESTION 1: ntasks appropriate amount? cpus per task? SEE NOTES FOLDER
-
 
 sample=$(awk "NR==${SLURM_ARRAY_TASK_ID}" /scratch/med68205/TRANS_CLOCK/usftp21.novogene.com/01.RawData/sample.names) #for each of the 85 samples
 
-
-#Make project directory + make directory for reference genome
-#Set directory to your master directory that everything is in 
 OUTDIR="/scratch/med68205/TRANS_CLOCK_PROCESSING" 
 
 if [ ! -d $OUTDIR/Genome ]
@@ -45,15 +40,50 @@ echo
 #load FastQC
 ml FastQC/0.11.9-Java-11
 
+
 echo 
 echo 'loading modules complete'
 echo
 
 
-fastqc -o $OUTDIR/RawFastQC/Logs -t 10 $OUTDIR/Data/${sample}/*.gz
+##fastqc -o $OUTDIR/RawFastQC/Logs -t 10 $OUTDIR/Data/${sample}/*.gz
 
 #load MultiQC
 ml MultiQC/1.14-foss-2022a
 
 #multiqc
-multiqc -o $OUTDIR/RawFastQC/MultiQC $OUTDIR/RawFastQC/Logs/*fastqc.zip
+#multiqc -o $OUTDIR/RawFastQC/MultiQC $OUTDIR/RawFastQC/Logs/*fastqc.zip
+
+echo 
+echo 'Load modules for trimming...'
+echo
+
+ml Trim_Galore/0.6.7-GCCcore-11.2.0
+ml Python/3.10.8-GCCcore-12.2.0
+ml pigz/2.7-GCCcore-11.3.0
+ml cutadapt/4.5-GCCcore-11.3.0
+
+##trim raw reads
+
+echo
+echo 'Trimming raw reads and performing FastQC...'
+echo
+
+#trim_galore --cores 4 --fastqc --fastqc_args "--outdir $OUTDIR/TrimmedQC" -stringency 3 -o $OUTDIR/TrimmedReads --paired $OUTDIR/Data/${sample}/${sample}_1.fq.gz $OUTDIR/Data/${sample}/${sample}_2.fq.gz
+fastqc -o $OUTDIR/TrimmedQC -t 10 $OUTDIR/TrimmedReads/*.gz
+
+echo
+echo 'trimming complete.'
+echo
+
+echo
+echo 'Concatenate all FastQC files using MultiQC...'
+echo
+
+multiqc /$OUTDIR/TrimmedQC
+
+date
+
+
+
+
